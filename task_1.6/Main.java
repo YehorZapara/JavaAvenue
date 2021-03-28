3fa28300270a6622fb1402b163e231f755467c35
YehorZapara <81438429+YehorZapara@users.noreply.github.com>
2021-03-28 15:30:23 +0300

    Create Main.java

---
+++ task_1.6/Main.java
@@ -0,0 +1,9 @@
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(20+20*0.15);
+    }
+}
